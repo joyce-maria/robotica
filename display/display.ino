c121a74161e3e9f9abd798deacf3b5c127b425cc
joyce-maria <126573182+joyce-maria@users.noreply.github.com>
2023-08-04 11:03:06 -0300

    Add files via upload

--- display/display.ino
+++ display/display.ino
@@ -1,27 +1,19 @@
-int potenciometro = 0;
+int Pino_buzzer = 11;
+void setup(){
 
-void setup() {
-  Serial.begin(9600);
-}
-void loop() {
-potenciometro = analogRead (A0);
-Serial.print("Leitura da porta:");
-Serial.println(potenciometro);
-delay(100);
-}
-int potenciametro = 0;
-int valorPWM;
-#define pin_led 3
-
-void setup() {
-  Serial.beguin(9600);
-  pinMode(pin_led, OUTPUT);
-}
-void loop() {
-  potenciometro = analogRead (A0);
-  Serial.print ("Leitura da porta:");
-  Serial.println(potenciamento);
-  delay(100);
-
-v
+  //tone(pin,frequency, duration)
+  
+  pinMode(Pino_buzzer,OUTPUT);
+  tone(Pino_buzzer, 264, 100); //DÓ
+  delay(250);
+  tone(Pino_buzzer, 297, 100); //RÉ
+  delay(250);
+  tone(Pino_buzzer, 330, 100); //MI
+  delay(250);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(500);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(200);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(500);
 }

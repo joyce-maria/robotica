87839e3c6ee33d564f50b26c186452de9e7b596b
joyce-maria <126573182+joyce-maria@users.noreply.github.com>
2023-08-18 10:56:40 -0300

    Add files via upload

---
+++ jmm2/robotica-main/display/display.ino
@@ -0,0 +1,63 @@
+int Pino_buzzer = 11;
+void setup(){
+
+  //tone(pin,frequency, duration)
+  
+  pinMode(Pino_buzzer,OUTPUT);
+  tone(Pino_buzzer, 264, 100); //DÓ
+  delay(250);
+  tone(Pino_buzzer, 297, 100); //RÉ
+  delay(250);
+  tone(Pino_buzzer, 330, 100); //MI
+  delay(250);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(500);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(200);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(500);
+
+  tone(Pino_buzzer, 264, 100); //DÓ
+  delay(250);
+  tone(Pino_buzzer, 297, 100); //RÉ
+  delay(250);
+  tone(Pino_buzzer, 269, 100); //DÓ
+  delay(250);
+  tone(Pino_buzzer, 297, 100); //RÉ
+  delay(500);
+  tone(Pino_buzzer, 297, 100); //RÉ
+  delay(200);
+  tone(Pino_buzzer, 297, 100); //RÉ
+  delay(500);
+
+  tone(Pino_buzzer, 264, 100); //DÓ
+  delay(250);
+  tone(Pino_buzzer, 396, 100); //SOL
+  delay(250);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(250);
+  tone(Pino_buzzer, 352, 100); //MI
+  delay(500);
+  tone(Pino_buzzer, 352, 100); //MI
+  delay(200);
+  tone(Pino_buzzer, 352, 100); //MI
+  delay(500);
+
+  tone(Pino_buzzer, 264, 100); //DÓ
+  delay(250);
+  tone(Pino_buzzer, 297, 100); //RÉ
+  delay(250);
+  tone(Pino_buzzer, 330, 100); //MI
+  delay(250);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(500);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(200);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(500);
+}
+
+void loop()
+{
+  
+}
---
+++ jmm2/robotica-main/jmm/robotica-main/display.ino
@@ -0,0 +1,94 @@
+int a = 13; // verde
+int b = 12; // amarelo
+int c = 11; // roxo
+int d = 10; // verde2
+int e = 9; // azul
+int f = 8; // azul2
+int g = 7;  // branco
+
+void setup(){
+  pinMode ( a, OUTPUT);
+  pinMode ( b, OUTPUT);
+  pinMode ( c, OUTPUT);
+  pinMode ( d, OUTPUT);  
+  pinMode ( e, OUTPUT);
+  pinMode ( f, OUTPUT);
+  pinMode ( g, OUTPUT);
+
+}
+
+void loop(){
+  digitalWrite (a, HIGH);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, HIGH);
+  delay(1000);
+
+  digitalWrite (a, LOW);
+  digitalWrite (b, LOW);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, LOW);
+  digitalWrite (e, LOW);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, LOW);
+  delay(1000);
+
+  digitalWrite (a, LOW);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, LOW);
+  delay(1000);
+
+  digitalWrite (a, HIGH);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, HIGH);
+  delay(1000);
+
+  digitalWrite (a, HIGH);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, HIGH);
+  delay(1000);
+
+  digitalWrite (a, HIGH);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, HIGH);
+  delay(1000);
+
+  digitalWrite (a, HIGH);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, HIGH);
+  delay(1000);
+
+  digitalWrite (a, HIGH);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, HIGH);
+  delay(1000);
+  
+
+}

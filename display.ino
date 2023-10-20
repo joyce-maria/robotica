fafbbfb5c1d49ea4c71e458300b76dd78ff81e6e
joyce-maria <126573182+joyce-maria@users.noreply.github.com>
2023-10-20 10:58:55 -0300

    20/10/2023

--- display.ino
+++ display.ino
@@ -1,94 +1,19 @@
-int a = 13; // verde
-int b = 12; // amarelo
-int c = 11; // roxo
-int d = 10; // verde2
-int e = 9; // azul
-int f = 8; // azul2
-int g = 7;  // branco
+int velocidade = 10;
+void setup() {
 
-void setup(){
-  pinMode ( a, OUTPUT);
-  pinMode ( b, OUTPUT);
-  pinMode ( c, OUTPUT);
-  pinMode ( d, OUTPUT);  
-  pinMode ( e, OUTPUT);
-  pinMode ( f, OUTPUT);
-  pinMode ( g, OUTPUT);
-
+  pinMode (3, OUTPUT);
+  pinMode (5, OUTPUT);
+  pinMode (6, OUTPUT);
+  pinMode (9, OUTPUT);
+  pinMode (10, OUTPUT);
+  
 }
 
-void loop(){
-  digitalWrite (a, HIGH);
-  digitalWrite (b, LOW);
-  digitalWrite (c, LOW);
-  digitalWrite (d, HIGH);
-  digitalWrite (e, HIGH);
-  digitalWrite (f, HIGH);
-  digitalWrite (g, HIGH);
-  delay(1000);
-
-  digitalWrite (a, LOW);
-  digitalWrite (b, LOW);
-  digitalWrite (c, HIGH);
-  digitalWrite (d, LOW);
-  digitalWrite (e, LOW);
-  digitalWrite (f, HIGH);
-  digitalWrite (g, LOW);
-  delay(1000);
-
-  digitalWrite (a, LOW);
-  digitalWrite (b, LOW);
-  digitalWrite (c, LOW);
-  digitalWrite (d, LOW);
-  digitalWrite (e, HIGH);
-  digitalWrite (f, HIGH);
-  digitalWrite (g, LOW);
-  delay(1000);
-
-  digitalWrite (a, HIGH);
-  digitalWrite (b, LOW);
-  digitalWrite (c, LOW);
-  digitalWrite (d, HIGH);
-  digitalWrite (e, HIGH);
-  digitalWrite (f, HIGH);
-  digitalWrite (g, HIGH);
-  delay(1000);
-
-  digitalWrite (a, HIGH);
-  digitalWrite (b, LOW);
-  digitalWrite (c, LOW);
-  digitalWrite (d, HIGH);
-  digitalWrite (e, HIGH);
-  digitalWrite (f, HIGH);
-  digitalWrite (g, HIGH);
-  delay(1000);
-
-  digitalWrite (a, HIGH);
-  digitalWrite (b, LOW);
-  digitalWrite (c, LOW);
-  digitalWrite (d, HIGH);
-  digitalWrite (e, HIGH);
-  digitalWrite (f, HIGH);
-  digitalWrite (g, HIGH);
-  delay(1000);
-
-  digitalWrite (a, HIGH);
-  digitalWrite (b, LOW);
-  digitalWrite (c, LOW);
-  digitalWrite (d, HIGH);
-  digitalWrite (e, HIGH);
-  digitalWrite (f, HIGH);
-  digitalWrite (g, HIGH);
-  delay(1000);
-
-  digitalWrite (a, HIGH);
-  digitalWrite (b, LOW);
-  digitalWrite (c, LOW);
-  digitalWrite (d, HIGH);
-  digitalWrite (e, HIGH);
-  digitalWrite (f, HIGH);
-  digitalWrite (g, HIGH);
-  delay(1000);
+void loop() {
+  analogWrite(3, 255);
+  analogWrite(5, 255);
+  analogWrite(6, 255);
+  analogWrite(9, 255);
+  analogWrite(10, 255);
   
-
 }
--- display/display.ino
+++ display/display.ino
@@ -16,4 +16,48 @@ void setup(){
   delay(200);
   tone(Pino_buzzer, 352, 100); //FÁ
   delay(500);
+
+  tone(Pino_buzzer, 264, 100); //DÓ
+  delay(250);
+  tone(Pino_buzzer, 297, 100); //RÉ
+  delay(250);
+  tone(Pino_buzzer, 269, 100); //DÓ
+  delay(250);
+  tone(Pino_buzzer, 297, 100); //RÉ
+  delay(500);
+  tone(Pino_buzzer, 297, 100); //RÉ
+  delay(200);
+  tone(Pino_buzzer, 297, 100); //RÉ
+  delay(500);
+
+  tone(Pino_buzzer, 264, 100); //DÓ
+  delay(250);
+  tone(Pino_buzzer, 396, 100); //SOL
+  delay(250);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(250);
+  tone(Pino_buzzer, 352, 100); //MI
+  delay(500);
+  tone(Pino_buzzer, 352, 100); //MI
+  delay(200);
+  tone(Pino_buzzer, 352, 100); //MI
+  delay(500);
+
+  tone(Pino_buzzer, 264, 100); //DÓ
+  delay(250);
+  tone(Pino_buzzer, 297, 100); //RÉ
+  delay(250);
+  tone(Pino_buzzer, 330, 100); //MI
+  delay(250);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(500);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(200);
+  tone(Pino_buzzer, 352, 100); //FÁ
+  delay(500);
+}
+
+void loop()
+{
+  
 }
